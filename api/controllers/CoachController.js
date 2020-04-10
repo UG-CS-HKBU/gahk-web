af28cfb78697105ae534fef68cff78d0befee3ef
Tina Ku <tinaku@Tinas-MacBook-Pro.local>
2020-04-10 19:37:01 +0800

    first stage of export

--- api/controllers/CoachController.js
+++ api/controllers/CoachController.js
@@ -367,4 +367,24 @@ module.exports = {
 
   },
 
+  export_xlsx: async function(req, res) {
+
+    var models = await Coach.find();
+    
+    var XLSX = require('xlsx');
+    var wb = XLSX.utils.book_new();
+    
+    var ws = XLSX.utils.json_to_sheet(models.map(model => {
+      return {
+        Engname: req.body.Coach.Engname,
+        ChiName: req.body.Coach.ChiName,
+        Id: req.body.Coach.Id,
+      }
+    }));
+    XLSX.utils.book_append_sheet(wb, ws, "coach");
+    
+    res.set("Content-disposition", "attachment; filename=coach.xlsx");
+    return res.end(XLSX.write(wb, {type:"buffer", bookType:"xlsx"}));
+  },
+
 };
