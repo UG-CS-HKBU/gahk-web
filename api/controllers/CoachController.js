6023fd5544e42945e0176231bf36300707fab9db
Tina Ku <tinaku@Tinas-MacBook-Pro.local>
2020-04-19 12:40:16 +0800

    chnage in status page

--- api/controllers/CoachController.js
+++ api/controllers/CoachController.js
@@ -109,6 +109,8 @@ module.exports = {
     }
   },
 
+
+
   cancel_coach: async function (req, res) {
     var year = new Date().getFullYear();
     var pid = parseInt(req.params.id) || -1;
@@ -285,12 +287,107 @@ module.exports = {
   },
 
 
+
+
+  csv: async function (req, res) {
+    var csv = require('fast-csv');
+    var arr = [];
+    var models = await Coach.find();
+    if (!models) { return res.notFound(); }
+
+    models.forEach(element => {
+      arr.push({
+
+        教練編號: 'INDC' + element.AtheleteNo,
+        申請類別: element.New_coach,
+
+        教練註冊級別: element.Level,
+
+        男子競技體操: element.Disciplines1,
+        女子競技體操: element.Disciplines2,
+        藝術體操: element.Disciplines3,
+        技巧體操: element.Disciplines4,
+        彈網: element.Disciplines5,
+        健美體操: element.Disciplines6,
+
+        英文名: element.Engname,
+        中文名: element.ChiName,
+
+        香港身份證號碼: element.Id,
+        性別: element.Gender,
+        出生日期: element.Birthd,
+
+        職業: element.Occupation,
+        學歷: element.Education,
+        住宅: element.Hnumber,
+        手提: element.Mnumber,
+        電郵地址: element.Email,
+        通訊地址: element.ChiAddress,
+
+        //
+        曾任教本會舉辦之課程: element.HaveBeenCoach,
+        如有: element.Have,
+        其他有關體操教學經驗: element.HaveBeenCoach1,
+        請列明: element.Have2,
+
+        //
+        本會發出之體操教練證書: element.Qualification,
+        證書編號: element.Cert_no,
+        考取年份: element.date_Qualification,
+
+        香港教練培訓委員會發出之證書: element.Qualification1,
+        認可教練號碼: element.Accredited_coachNo,
+        有效日期: element.date_Qualification1,
+        //
+        本會發出之教練理論課程證書: element.Qualification2,
+        證書編號: element.Cert_no2,
+        有效日期: element.date_Qualification2,
+
+
+        急救證書: element.Qualification3,
+        發出機構: element.Issued_by,
+        考取年份: element.date_Qualification3,
+
+
+        其他有關體操教練資歷: element.Qualification4,
+        原因: element.others,
+        是否已考取本地國際裁判資歷: element.judges,
+        考取年份: element.date_qualification5,
+
+        //
+
+        曾於學校或其他地區教授之訓練班舉辦日期: element.date_training,
+        學校機構名稱: element.school_training,
+        教授項目: element.Discipline_training,
+
+
+        曾教導學生考取章別計劃之數量舉辦日期: element.Badges_date,
+        訓練班學校機構名稱: element.Badges_school,
+        級別及數量: element.Badges_no,
+
+        曾參與總會之體操推廣活動舉辦日期: element.Activities_date,
+        活動名稱: element.Activities_event,
+        負責職位: element.Activities_badges,
+
+        曾參與總會及香港教練培訓委員會之複修課程舉辦日期: element.Coaching_workshops_date,
+        活動名稱: element.Coaching_workshops_event,
+        舉辦機構: element.Coaching_workshops_organization,
+        聲明: element.hope,
+
+      });
+
+    });
+
+    res.set('Content-type', 'text/csv');
+    csv.write(arr, { headers: true }).pipe(res);
+  },
+
   coachform_detail: async function (req, res) {
 
     var coach = await Coach.findOne(req.params.id);
-    if (req.method == 'GET')
-    {return res.view('membership/coachform_detail', { 'coach': coach, 'reg': 0 });}
+    if (req.method == 'GET') { return res.view('membership/coachform_detail', { 'coach': coach, 'reg': 0 }); }
   },
+
 
   canel_coach: async function (req, res) {
     var pid = parseInt(req.params.id) || -1;
@@ -299,16 +396,14 @@ module.exports = {
 
     }).fetch();
 
-    return res.redirect('/coach/coach_record');
+    if (req.wantsJSON) {
+      return res.json({ message: "reject successfully！", url: '/status' });    // for ajax request
+    }
 
-        // 男子競技體操: element.Disciplines1,
-        // 女子競技體操: element.Disciplines2,
-        // 藝術體操: element.Disciplines3,
-        // 技巧體操: element.Disciplines4,
-        // 彈網: element.Disciplines5,
-        // 健美體操: element.Disciplines6,
+    //return res.redirect('/coach/coach_record');
+
   },
-  
+
   status: async function (req, res) {
     var coach = await Coach.find();
     return res.view('profile/status', { 'coach': coach });
@@ -321,7 +416,7 @@ module.exports = {
     var XLSX = require('xlsx');
     var wb = XLSX.utils.book_new();
     
-    var ws = XLSX.utils.json_to_sheet(models.filter(model=>model.confirm_coach=="是").map(model => {
+    var ws = XLSX.utils.json_to_sheet(models.filter(model=>model.comfirm_coach=="是").map(model => {
       return {
         
         教練編號: 'INDC' + model.CoachNo,
@@ -404,7 +499,7 @@ module.exports = {
       }     
     }));
 
-       var ws1 = XLSX.utils.json_to_sheet(models.filter(model=>model.confirm_coach!="是" && model.confirm_coach!="否").map(model => {
+       var ws1 = XLSX.utils.json_to_sheet(models.filter(model=>model.comfirm_coach=="").map(model => {
       return {
         
         教練編號: 'INDC' + model.CoachNo,
@@ -493,5 +588,7 @@ module.exports = {
     return res.end(XLSX.write(wb, {type:"buffer", bookType:"xlsx"}));
     
   },
+
+
 
 };
